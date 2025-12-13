c9982de8c4ea54a5c33d505543da334b7867b338
jafar <jafarzhairy5@gmail.com>
2025-12-13 18:26:25 +0300

    the orginal project

---
+++ lib/constance/button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class button extends StatefulWidget {
+  const button({super.key});
+
+  @override
+  State<button> createState() => _buttonState();
+}
+
+class _buttonState extends State<button> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(onPressed:(){} ,child: ,);
+  }
+}
