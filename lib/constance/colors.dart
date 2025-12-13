fd38c7746d7f0193846fddfe6850576a6289d386
adam <adamabdmohamad>
2025-12-13 23:28:11 +0300

    set colors

--- lib/constance/colors.dart
+++ lib/constance/colors.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-Color primryColor = Color(0xff);
-Color SecondryColor = Color(0xff);
-Color pimryColor2 = Color(0xff);
-Color secondryColor2 = Color(0xff);
-Color available  = Color(0xff);
-Color notAvailable = Color(0xff);
-Color defaultColor = Color(0xff);
-
+Color primryColor = Color(0xff131925);
+Color SecondryColor = Color(0xffC7B07B);
+Color pimryColor2 = Color(0xffD3D3D3);
+Color secondryColor2 = Color(0xff131925);
+Color available = Color(0xff245912);
+Color notAvailable = Color(0xff561919);
+Color defaultColor = Color(0xffFFFFFF);
